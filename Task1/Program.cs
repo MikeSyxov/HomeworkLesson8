46685d70012d2e5340692489439ed2c2613c6e9a
Mikhail T <Mikola301@mail.ru>
2023-07-08 14:01:18 +0700

    added task1 и readme

---
+++ Task1/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочивает по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateArrD2(int rows, int cols)
+{
+    int[,] arrayD2 = new int[rows, cols];
+
+    for (int i = 0; i < arrayD2.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayD2.GetLength(1); j++)
+        {
+            arrayD2[i, j] = new Random().Next(1, 9);
+        }
+    }
+    return arrayD2;
+}
+
+void ShowArrayD2(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedString = Console.ReadLine();
+    if (int.TryParse(inputedString, out int convertedInt))
+    {
+        return convertedInt;
+    }
+
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int[,] SortRowsMin(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1) - 1; k++)
+            {
+                if (arr[i, k] < arr[i, k + 1])
+                {
+                    int temp = arr[i, k + 1];
+                    arr[i, k + 1] = arr[i, k];
+                    arr[i, k] = temp;
+                }
+            }
+        }
+    }
+    return arr;
+}
+int rows = ReadInt("Введите количестыо строк");
+int cols = ReadInt("Введите количество столбцов");
+int[,] array = CreateArrD2(rows, cols);
+ShowArrayD2(array);
+System.Console.WriteLine();
+ShowArrayD2(SortRowsMin(array));
